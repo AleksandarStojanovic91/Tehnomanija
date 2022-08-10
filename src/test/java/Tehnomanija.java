ac5578c42dffb9702e01419dba8cf5c41a3e86eb
lcedomir <cedomirlukic@gmail.com>
2022-08-10 23:36:30 +0200

    test cedomir 2

--- src/test/java/Tehnomanija.java
+++ src/test/java/Tehnomanija.java
@@ -35,7 +35,7 @@ public class Tehnomanija {
     public void tearDown() {
 //        driver.quit();
     }
-
+cedomir test
     @Test(description = "Ime testa")
     @Description("Opis testa")
     @Epic("EP-001")
